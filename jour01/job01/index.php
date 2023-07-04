201ad178e90307e47fa9aad97c9e39ae168f0016
clement-machtelinckx <clement.machtelinckx@laplateforme.io>
2023-07-04 09:06:17 +0200

    1 a 4 done

---
+++ jour01/job01/index.php
@@ -0,0 +1,17 @@
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="" media="screen">
+    <script>
+        window.onload = function() {
+            alert("Hello World");
+        };
+    </script>
+    <title>job01</title>
+</head>
+
+<body>
+
+
+</body>
